3b11b99dc3cba72ea15d760a4bccda3fe0b6a99c
Lucas  Sköld <lucasskold49@gmail.com>
2024-06-21 12:45:44 +0200

    Added eureka server

--- src/main/java/com/example/wigellssushi/WigellssushiApplication.java
+++ src/main/java/com/example/wigellssushi/WigellssushiApplication.java
@@ -2,8 +2,10 @@ package com.example.wigellssushi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WigellssushiApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/wigellssushi/WigellssushiApplicationTests.java
+++ src/test/java/com/example/wigellssushi/WigellssushiApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.wigellssushi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WigellssushiApplicationTests {
 
 	@Test
